972556406a7f73010bf966839f8335a05f43f35a
aloks <sarkarsworld@gmail.com>
2023-11-08 18:05:57 +0530

    Initial project structure created.

---
+++ src/main/java/com/simplesolutions/SpringSecurityJwtauthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.simplesolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtauthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtauthenticationApplication.class, args);
+	}
+
+}
